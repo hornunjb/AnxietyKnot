2cdaa87a3362c72d7c9b3cb4e9eac4c4b72a465a
OliverPo666 <55895574+OliverPo666@users.noreply.github.com>
2022-01-23 17:26:05 -0500

    now the after login, it will direct to the nav component and the home page will be the deafult page

--- AnxietyKnot/src/app/login/login.component.ts
+++ AnxietyKnot/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginObj: any;
+  constructor(private router: Router) { 
+    this.loginObj ={
+      userName:'',
+      password:''
+    };
+  }
 
   ngOnInit(): void {
+  }
+  login(){
+    if(this.loginObj.userName == 'admin' && this.loginObj.password == '1234'){
+      this.router.navigateByUrl('home')
+    }
+    else{
+      alert('Wrong Crendertials');
+    }
   }
 
 }

007ad3cfc3737918a787745a6d79bea51c8436b1
Jacob Hornung <hornunjb@mail.uc.edu>
2022-02-24 21:04:37 -0500

    different tips for posts length

--- AnxietyKnot/src/app/journal-book/journal-book.component.ts
+++ AnxietyKnot/src/app/journal-book/journal-book.component.ts
@@ -1,40 +1,36 @@
-import { Component, Input, OnInit, OnDestroy } from "@angular/core";
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { Post } from "../post.model";
-import { PostsService } from "../posts.service";
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 const allowedEntryLength = 500;
-
-
 
 @Component({
   selector: 'app-journal-book',
   templateUrl: './journal-book.component.html',
-  styleUrls: ['./journal-book.component.css']
+  styleUrls: ['./journal-book.component.css'],
 })
 export class JournalBookComponent {
-
-
-
   posts: Post[] = [];
   public noHtmlContent: string[] = [];
-  private postsSub: Subscription = new Subscription;
+  private postsSub: Subscription = new Subscription();
+  tip: string = '';
 
   constructor(public postsService: PostsService) {}
 
   ngOnInit() {
     this.postsService.getPosts();
-    this.postsSub = this.postsService.getPostUpdateListener()
+    this.postsSub = this.postsService
+      .getPostUpdateListener()
       .subscribe((posts: Post[]) => {
         this.posts = posts;
       });
-
-    }
+  }
 
   replace(content: any) {
     var parsedContent = content.replace(/<[^>]+>/g, '');
     if (parsedContent.length > allowedEntryLength) {
-      return parsedContent.slice(0, allowedEntryLength) + "...";
+      return parsedContent.slice(0, allowedEntryLength) + '...';
     }
     return parsedContent;
   }
@@ -48,9 +44,20 @@ export class JournalBookComponent {
   }
 
   setEntryLength(entryContent) {
-      entryContent.truncate(250);
-      return entryContent;
+    entryContent.truncate(250);
+    return entryContent;
   }
 
+  tipMaker() {
+    if (this.posts.length > 0) {
+      if (this.posts.length > 10) {
+        this.tip = "Develop a routine so that you're physically active most days of the week. Exercise is a powerful stress reducer. It can improve your mood and help you stay healthy. Start out slowly, and gradually increase the amount and intensity of your activities";
+        return true;
+      } else {
+        this.tip = "Nicotine and caffeine can worsen anxiety.";
+        return true;
+      }
+    }
+    return false;
+  }
 }
-
